cb0afc375cd98164f0ee495543d9a2013c72bf0a
arifsptra <arif.sptrra@gmail.com>
2023-07-01 00:00:55 +0700

    separate style and script code

---
+++ js/tailwind.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#0088b9",
+          textOne: "#111",
+          textTwo: "#333",
+          textThree: "#555",
+        },
+      },
+    },
+  };
